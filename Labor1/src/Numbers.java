6f844b2df61993801c057d9509b2906c90f180ed
demkudominik <demkudominik@outlok.com>
2024-10-15 20:13:51 +0300

    Implement minimal Number Method

---
+++ Labor1/src/Numbers.java
@@ -0,0 +1,13 @@
+public class Numbers {
+
+    private final int[] number;
+    public Numbers(int...numbers) {this.number = numbers;}
+
+    public int minimalNumber(){
+        int minimalNumber = number[0];
+        for (Integer number: number)
+            if (minimalNumber > number)
+                minimalNumber = number;
+        return minimalNumber;
+    }
+}
