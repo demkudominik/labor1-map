473389266cf2d0c2ab702776b40c285967e5790c
demkudominik <demkudominik@outlok.com>
2024-10-15 18:27:44 +0300

    Implement Insufficient Grades Method

---
+++ Labor1/src/Universitat.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class Universitat {
+
+
+    public int nextMultipleOf5(int number) {
+        return number / 5 * 5 + 5;
+    }
+
+    public int round(int grade) {
+        if (grade != 100 && nextMultipleOf5(grade) - grade < 3) {
+            return nextMultipleOf5(grade);
+        }
+        return grade;
+    }
+
+    public boolean isInsufficient(int grade) {
+        return (grade < 38 || round(grade) < 40);
+    }
+
+    public ArrayList<Integer> insufficientGrades(ArrayList<Integer> grades) {
+        ArrayList<Integer> result = new ArrayList<>();
+        for (Integer grade : grades) {
+            if (isInsufficient(grade)) {
+                result.add(grade);
+            }
+        }
+        return result;
+    }
+
+
+
+
+
+
+}
