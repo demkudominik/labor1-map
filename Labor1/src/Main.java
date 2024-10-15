733039ca4af1107be35bee976f4657422a923648
demkudominik <demkudominik@outlok.com>
2024-10-15 21:19:38 +0300

    Create Test for Numbers as Array Class

--- Labor1/src/Main.java
+++ Labor1/src/Main.java
@@ -21,8 +21,13 @@ public class Main {
         System.out.println(maximalRoundedGrade);
 
         System.out.println("\n aufgabe2");
-
         numberEx();
+
+        System.out.println("\n aufgabe3");
+        ArrayEx1();
+        ArrayEx2();
+        ArrayEx3();
+        ArrayEx4();
     }
 
     public static void numberEx() {
@@ -40,5 +45,48 @@ public class Main {
         int sumWithoutMaximalNumber = numbers.sumWithoutMaximalNumber();
         System.out.println(sumWithoutMaximalNumber);
 
+    }
+
+    public static void ArrayEx1() {
+
+        NumbersAsArray num1 = new NumbersAsArray(1, 3, 0, 0, 0, 0, 0, 0, 0);
+        NumbersAsArray num2 = new NumbersAsArray(8, 7, 0, 0, 0, 0, 0, 0, 0);
+
+        int[] additionResult = num1.add(num2);
+        for (int i : additionResult) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+        public static void ArrayEx2() {
+        NumbersAsArray num1 = new NumbersAsArray(8, 3, 0, 0, 0, 0, 0, 0, 0);
+        NumbersAsArray num2 = new NumbersAsArray(5, 4, 0, 0, 0, 0, 0, 0, 0);
+
+        int[] subtractionResult = num1.sutract(num2);
+        for (int i : subtractionResult) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void ArrayEx3() {
+
+        NumbersAsArray num = new NumbersAsArray(2, 3, 6, 0, 0, 0, 0, 0, 0);
+        int[] multiplicationResult = num.multiply(2);
+        for (int i : multiplicationResult) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void ArrayEx4() {
+
+        NumbersAsArray num = new NumbersAsArray(2, 3, 6, 0, 0, 0, 0, 0, 0);
+        int[] divisionResult = num.divide(2);
+        for (int i : divisionResult) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
     }
 }
