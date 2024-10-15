048f5fea276492903bf486449dd6cbcf59ea4b72
demkudominik <demkudominik@outlok.com>
2024-10-15 20:19:39 +0300

    Implement sum and sum without minimal/maximal numeber Method

--- Labor1/src/Numbers.java
+++ Labor1/src/Numbers.java
@@ -1,21 +1,34 @@
 public class Numbers {
 
     private final int[] number;
-    public Numbers(int...numbers) {this.number = numbers;}
 
-    public int minimalNumber(){
+    public Numbers(int... numbers) {
+        this.number = numbers;
+    }
+
+    public int minimalNumber() {
         int minimalNumber = number[0];
-        for (Integer number: number)
+        for (Integer number : number)
             if (minimalNumber > number)
                 minimalNumber = number;
         return minimalNumber;
     }
 
-    public int maximumNumber(){
+    public int maximumNumber() {
         int maximumNumber = number[0];
-        for(Integer number: number)
+        for (Integer number : number)
             if (maximumNumber < number)
                 maximumNumber = number;
         return maximumNumber;
     }
+
+    public int sum() {
+        int sum = 0;
+        for (Integer number : number)
+            sum += number;
+        return sum;
+    }
+
+    public int sumWithoutMinimalNumber(){return sum() - minimalNumber();}
+    public int sumWithoutMaximalNumber(){return sum() - maximumNumber();}
 }
