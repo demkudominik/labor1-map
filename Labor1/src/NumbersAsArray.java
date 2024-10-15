608fc5a58cfca52b20c7aab87ca2146913ef3918
demkudominik <demkudominik@outlok.com>
2024-10-15 20:48:16 +0300

    Implement Array to Number and Number to Array Method

---
+++ Labor1/src/NumbersAsArray.java
@@ -0,0 +1,30 @@
+public class NumbersAsArray {
+
+    private final int[] digits;
+
+    public int[] get (){return digits;}
+
+    public NumbersAsArray (int...digits){ this.digits = digits;}
+
+
+    public int arrayToNumber(int[] numberAsArray){
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int digit : numberAsArray){
+            stringBuilder.append(digit);
+        }
+        return Integer.parseInt(stringBuilder.toString());
+    }
+
+    public int[] numberToArray(int number){
+        int numberOfDigits = (int) Math.log10(number) + 1;
+
+        int[] result = new int[numberOfDigits];
+        for(int i = numberOfDigits - 1; i >= 0; i--){
+            result[i] = number % 10;
+            number /= 10;
+        }
+        return result;
+    }
+
+
+}
