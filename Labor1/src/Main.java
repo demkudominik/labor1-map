c85e78d8ceabb18622b80fec6c90425391c941f9
demkudominik <demkudominik@outlok.com>
2024-10-15 20:29:45 +0300

    Create Test for Numbers Class

--- Labor1/src/Main.java
+++ Labor1/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Universitat universitat = new Universitat();
 
-        ArrayList<Integer> grades= new ArrayList<>(Arrays.asList(29, 37, 38, 41, 84, 67));
+        ArrayList<Integer> grades = new ArrayList<>(Arrays.asList(29, 37, 38, 41, 84, 67));
 
         ArrayList<Integer> insufficientGrades = universitat.insufficientGrades(grades);
         System.out.println(insufficientGrades);
@@ -19,6 +19,26 @@ public class Main {
 
         int maximalRoundedGrade = universitat.maximalRoundedGrade(grades);
         System.out.println(maximalRoundedGrade);
+
+        System.out.println("\n aufgabe2");
+
+        numberEx();
     }
 
+    public static void numberEx() {
+        Numbers numbers = new Numbers(4, 8, 3, 10, 17);
+
+        int maximalNumber = numbers.maximumNumber();
+        System.out.println(maximalNumber);
+
+        int minimalNumber = numbers.minimalNumber();
+        System.out.println(minimalNumber);
+
+        int sumWithoutMinimalNumber = numbers.sumWithoutMinimalNumber();
+        System.out.println(sumWithoutMinimalNumber);
+
+        int sumWithoutMaximalNumber = numbers.sumWithoutMaximalNumber();
+        System.out.println(sumWithoutMaximalNumber);
+
+    }
 }
